4be65d5a7be629fe40bc641f57bad436a22949c9
alberyan <albert-yyh@163.com>
2019-04-10 14:40:15 +0800

    Create 319. Bulb Switcher.java

---
+++ 319. Bulb Switcher.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        return (int)(Math.sqrt(n));
+    }
+}
+
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for Bulb Switcher.
+// Memory Usage: 31.7 MB, less than 100.00% of Java online submissions for Bulb Switcher.

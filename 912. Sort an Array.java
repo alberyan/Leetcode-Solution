d021ad31e8f98c06fa6d539ff248b967ffaa5bff
alberyan <albert-yyh@163.com>
2019-04-24 11:12:34 +0800

    Create 912. Sort an Array.java

---
+++ 912. Sort an Array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+}
+
+// Runtime: 5 ms, faster than 100.00% of Java online submissions for Sort an Array.
+// Memory Usage: 45 MB, less than 100.00% of Java online submissions for Sort an Array.

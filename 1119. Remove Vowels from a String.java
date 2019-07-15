68a40446a90579cc568e5c0ac92dddc8c185a375
alberyan <alberyan@cisco.com>
2019-07-15 11:29:26 +0800

    Create 1119. Remove Vowels from a String.java

---
+++ 1119. Remove Vowels from a String.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String removeVowels(String S) {
+        return S.replaceAll("[aeiou]", "");
+    }
+}
+
+// Runtime: 2 ms, faster than 100.00% of Java online submissions for Remove Vowels from a String.
+// Memory Usage: 34.6 MB, less than 100.00% of Java online submissions for Remove Vowels from a String.

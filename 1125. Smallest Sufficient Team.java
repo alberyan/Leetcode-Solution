c9ddc7eea0254be2ab5504a209079c6589786f6e
alberyan <alberyan@cisco.com>
2019-08-05 11:21:06 +0800

    Create 1125. Smallest Sufficient Team.java

---
+++ 1125. Smallest Sufficient Team.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] smallestSufficientTeam(String[] req_skills, List<List<String>> people) {
+        int sLen = req_skills.length;
+        int pLen = people.size();
+        
+        Map<String, Integer> skmap = new HashMap<>();
+        for(int i = 0; i < sLen; i++)
+            skmap.put(req_skills[i], i);
+        
+        Set<Integer>[] skillArr = new Set[1 << sLen];
+        skillArr[0] = new HashSet<>();
+        
+        for(int ppl = 0; ppl < pLen; ppl++){
+            int pplSkill = 0;
+            for(String sks : people.get(ppl)){
+                pplSkill |= 1 << (skmap.get(sks));
+            }
+            
+            for(int k = 0; k < skillArr.length; k++){
+                if(skillArr[k] == null) continue;
+                Set<Integer> currSkills = skillArr[k];
+                int combined = k | pplSkill;
+                if(combined == k) continue;
+                if(skillArr[combined] == null || skillArr[combined].size() > currSkills.size() + 1){
+                    Set<Integer> cSkills = new HashSet<>(currSkills);
+                    cSkills.add(ppl);
+                    skillArr[combined] = cSkills;
+                }
+            }
+        }
+        
+        Set<Integer> resSet = skillArr[(1 << sLen) - 1];
+        int[] res = new int[resSet.size()];
+        int pos = 0;
+        for(Integer n : resSet)
+            res[pos++] = n;
+        
+        return res;
+    }
+}
+
+// Runtime: 57 ms, faster than 69.50% of Java online submissions for Smallest Sufficient Team.
+// Memory Usage: 53.2 MB, less than 100.00% of Java online submissions for Smallest Sufficient Team.

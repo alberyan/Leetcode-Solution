f924e36b097c7c94782207680f66bd4bc2137f49
alberyan <alberyan@cisco.com>
2020-07-06 18:53:22 +0800

    Create 30Days - Reverse String.java

---
+++ 30Days - Reverse String.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            swap(s, left, right);
+            left++;
+            right--;
+        }
+    }
+    
+    private void swap(char[] s, int left, int right) {
+        char tmp = s[left];
+        s[left] = s[right];
+        s[right] = tmp;
+    }
+}
+
+//ACC

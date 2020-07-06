3d31c3e0637e4f466ce4f31cd15dedec5ddcce92
alberyan <alberyan@cisco.com>
2020-07-06 18:54:02 +0800

    Create 636. Exclusive Time of Functions.java

---
+++ 636. Exclusive Time of Functions.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        int[] res = new int[n];
+        Set<
+        for (String log: logs){
+        	String[] strArr = log.split(":");
+        	int index = Integer.parseInt(strArr[0]);
+        	int time = Integer.parseInt(strArr[2]);
+        }
+    }
+}

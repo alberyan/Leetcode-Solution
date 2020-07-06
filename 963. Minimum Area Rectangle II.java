41d36aa4d6023bbb61dcfa67cce25e0675459edf
alberyan <alberyan@cisco.com>
2020-07-06 18:54:13 +0800

    Create 963. Minimum Area Rectangle II.java

---
+++ 963. Minimum Area Rectangle II.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double minAreaFreeRect(int[][] points) {
+        double res = Integer.MAX_VALUE;
+        for (int i = 0; i < points.length; i++){
+            for (int j = i + 1; j < points.length; j++){
+                for (int k = j + 1; k < points.length; k++){
+                    
+                }
+            }
+        }
+        return res == Integer.MAX_VALUE? 0: res;
+    }
+}

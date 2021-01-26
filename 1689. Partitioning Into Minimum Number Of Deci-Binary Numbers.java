57552a60a360bcc5ba0c57f52f63fec3fc58bd1c
alberyan <alberyan@cisco.com>
2021-01-26 10:43:02 +0800

    Create 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.java

---
+++ 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minPartitions(String n) {
+        int res = 0;
+        for (char ch: n.toCharArray()) {
+            res = Math.max(res, ch - '0');
+        }
+        return res;
+    }
+}
